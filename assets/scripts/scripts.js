3b81728147c1851bf1ce0c006703049b6dd30512
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-02 16:29:53 +0200

    ajout du numéro du tour dans l'historique de combat

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -11,24 +11,23 @@ form.addEventListener('submit', (e) => {
     //permet d'arreter la soumission du formulaire
     e.preventDefault();
     // c'est là que ça démarre
-    let compteurTour = 0;
+    let compteurTour = 1;
 
     while (lifeWizard1 > 0 && lifeWizard2 > 0){
         if (compteurTour%2 === 0){
             let damage = attack();
             lifeWizard2 -= damage;
-            fightDisplay(wizardName1.value, wizardName2.value, damage, lifeWizard2);
+            fightDisplay(compteurTour, wizardName1.value, wizardName2.value, damage, lifeWizard2);
         }else{
             let damage = attack();
             lifeWizard1 -= damage;
-            fightDisplay(wizardName2.value, wizardName1.value, damage, lifeWizard1);
+            fightDisplay(compteurTour, wizardName2.value, wizardName1.value, damage, lifeWizard1);
         }
         compteurTour ++;
-        console.log(wizardName1);
-        // console.log(lifeWizard2);
-        // console.log(compteurTour);
     }
+    
 })
+
 /*
 fonction qui génère la valeur de l'attaque à chaque tour. Cette
 valeur un entier dans l'intervalle [5 ; 16[
@@ -37,8 +36,8 @@ function attack (min = 5, max = 16){
     let attackForce = Math.floor(Math.random() * (max-min) + min);
     return attackForce
 }
-function fightDisplay(firstWizardName, secondWizardName, force, life){
+function fightDisplay(tour, firstWizardName, secondWizardName, force, life){
     let paraFightRecord = document.createElement("p");
-    paraFightRecord.innerHTML = `${firstWizardName} attaque ${secondWizardName} pour ${force} dégâts. Il reste ${life} à ${secondWizardName}`;
+    paraFightRecord.innerHTML = `Tour ${tour} : ${firstWizardName} attaque ${secondWizardName} pour ${force} dégâts. Il reste ${life} à ${secondWizardName}`;
     fightRecord.insertAdjacentElement("beforeend", paraFightRecord);
 }
