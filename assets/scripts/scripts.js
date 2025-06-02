4a249f84717e6deed57c9e17f76eedc40843fdc7
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-02 14:58:09 +0200

    creation du fichier script

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,8 @@
+let form = document.querySelector(`#formulaire`);
+
+form.addEventListener('submit', (e) => {
+    //permet d'arreter la soumission du formulaire
+    e.preventDefault();
+    
+
+})
