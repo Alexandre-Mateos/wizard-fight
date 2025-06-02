8f831c89de03c3cb626a27a9db96b7045bb24b86
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-02 16:03:46 +0200

    création de la boucle de jeu

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,8 +1,27 @@
 let form = document.querySelector(`#formulaire`);
 
+let lifeWizard1 = 200;
+let lifeWizard2 = 200;
+
 form.addEventListener('submit', (e) => {
     //permet d'arreter la soumission du formulaire
     e.preventDefault();
-    
-
+    // c'est là que ça démarre
+    let compteurTour = 0;
+    while (lifeWizard1 > 0 && lifeWizard2 > 0){
+        if (compteurTour%2 === 0){
+            lifeWizard2 -= attack();
+        }else{
+            lifeWizard1 -= attack ();
+        }
+        compteurTour ++;
+        console.log(lifeWizard1);
+        console.log(lifeWizard2);
+        console.log(compteurTour);
+    }
 })
+
+function attack (min = 5, max = 16){
+    let attackForce = Math.floor(Math.random() * (max-min) + min);
+    return attackForce
+}
