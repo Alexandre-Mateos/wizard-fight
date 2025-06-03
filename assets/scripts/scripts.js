e32506116e154bea3df599faab6a4a1a4465b8af
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-03 11:44:56 +0200

    affichage des deux duelistes, et affichage de leur vie mise à jour à chaque tour

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,6 +5,15 @@ let wizardName2 = document.querySelector(`#wizardName2`);
 let wizardHouse1 = document.querySelector(`#maison1`);
 let wizardHouse2 = document.querySelector(`#maison2`);
 
+// elements pour l'affichage des deux sorciers duelistes
+let duelistName1 = document.querySelector("#duelistName1");
+let duelistHouse1 = document.querySelector("#duelistHouse1");
+let duelistLife1 = document.querySelector("#duelistLife1");
+
+let duelistName2 = document.querySelector("#duelistName2");
+let duelistHouse2 = document.querySelector("#duelistHouse2");
+let duelistLife2 = document.querySelector("#duelistLife2");
+
 /*
 Objet qui stocke les infos de mes sorciers:
 [nom, maison, vie restante, force]
@@ -29,11 +38,20 @@ form.addEventListener("submit", (e) => {
   wizard1.house = wizardHouse1.value;
   wizard2.house = wizardHouse2.value;
 
-  // boucle de jeu
-  let compteurTour = 1;
+  // Affichage des deux sorciers
+  duelistName1.innerHTML = wizard1.name;
+  duelistHouse1.innerHTML = wizard1.house;
+  duelistLife1.innerHTML = wizard1.life;
+
+  duelistName2.innerHTML = wizard2.name;
+  duelistHouse2.innerHTML = wizard2.house;
+  duelistLife2.innerHTML = wizard2.life;
+ 
   
 
- let fightLoop = setInterval(() => {
+  // boucle de jeu
+  let compteurTour = 1;
+  let fightLoop = setInterval(() => {
     if (compteurTour % 2 === 0) {
       attack(wizard1, wizard2);
       fight(compteurTour, wizard1, wizard2);
@@ -41,15 +59,11 @@ form.addEventListener("submit", (e) => {
       attack(wizard2, wizard1);
       fight(compteurTour, wizard2, wizard1);
     }
-    if(wizard1.life <=0 || wizard2.life <= 0){
+    if (wizard1.life <= 0 || wizard2.life <= 0) {
       clearInterval(fightLoop);
     }
     compteurTour++;
- }, 500);
-
-
-
-
+  }, 300);
 });
 
 /*
@@ -69,11 +83,12 @@ function attack(wizardAttacking, wizardDefending, min = 5, max = 15) {
 /*
 fonction qui affiche les tours du combat
 */
-function fight(tour, wizardAttacking, wizardDefending, min = 5, max = 15) {
-  // affichage des coups
+function fight(tour, wizardAttacking, wizardDefending) {
   let paraFight = document.createElement("p");
   paraFight.innerHTML = `Tour ${tour} : ${wizardAttacking.name} attaque ${wizardDefending.name} pour ${wizardAttacking.attackPower} dégâts. Il reste ${wizardDefending.life} PV à ${wizardDefending.name}`;
   fightRecord.insertAdjacentElement("beforeend", paraFight);
+  duelistLife1.innerHTML = wizard1.life;
+  duelistLife2.innerHTML = wizard2.life;
 }
 
 
